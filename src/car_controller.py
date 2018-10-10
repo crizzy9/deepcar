52a33d525def264d72c1875e7f8636c3017a453e
exMachina <shyampadia@live.com>
2018-10-09 19:49:36 -0700

    car controller skeleton added

---
+++ src/car_controller.py
@@ -0,0 +1,17 @@
+import requests
+
+class CarController:
+    HOST = 'http://raspberrypi.local'
+    PORT = '8000'
+    BASE_URL = HOST + PORT
+
+    def __init__(self):
+        
+    #login
+
+    def change_direction():
+
+    def change_speed():
+
+    def move_cam():
+
