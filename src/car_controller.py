a76983963ce2e4dbc0b7623b9f4fc15142d93b3e
Shyam Padia <shyampadia@live.com>
2018-10-13 00:06:39 -0700

    skeleton expanded

--- src/car_controller.py
+++ src/car_controller.py
@@ -11,7 +11,17 @@ class CarController:
 
     def change_direction():
 
-    def change_speed():
+    def change_speed(speed):
+    """
+    :param speed: The speed to be set
+    :type speed: int, float
+    :where: speed is in range (0, 100)
+    """
 
     def move_cam():
 
+    def check_connection():
+    """
+    :return: True if connection ok, false if no connection
+    :rtype: bool
+    """
